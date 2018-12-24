639dc36713c393c97f785a00ff5c15f23cde9eb3
TaegyunHa <34822734+TaegyunHa@users.noreply.github.com>
2018-12-24 19:16:19 +0000

    Create Smart_ptr.cpp

---
+++ Cpp/Smart_ptr.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<memory>
+#include<vector>
+
+using namespace std;
+
+class test1 {
+public:
+	int a;
+	test1() :a{ 10 } { cout << "test1 constructor, a: " << a << endl; }
+	~test1() { cout << "test1 destructor, a: " << a << endl; }
+	void increment() { cout << "increment() a: " << a++ << endl;; }
+};
+
+class test2 {
+public:
+	test2() { cout << "test2 constructor" << endl; }
+	~test2() { cout << "test2 destructor" << endl; }
+	static unique_ptr<test1> unq_ptr;
+};
+
+unique_ptr<test1> test2::unq_ptr = make_unique<test1>();
+
+int main()
+{
+	unique_ptr<test2> ptr_test = make_unique<test2>();
+	cout << ptr_test->unq_ptr->a << endl;
+	ptr_test->unq_ptr->increment();
+	cout << ptr_test->unq_ptr->a << endl;
+
+}
